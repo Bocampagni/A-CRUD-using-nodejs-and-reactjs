de28849d868227d0c5e0bf56e5d692f0ddf66b5e
Fabio Bocampagni <a.bocampagni@gmail.com>
2020-02-05 18:55:15 -0300

    Started

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-height = 1.76;
-weight = 105;
+const arr = [1, 2, 3, 4];
 
+const newArr = arr.map(function(item) {
+    return item * 2;
+}) 
 
-imc = weight / (height * height);
-
-
-if(imc < 10){
-    console.log("Desnutrição")
-}
-else if(imc == 10 || imc < 18.5){
-    console.log("Normal");
-}
-else if( imc == 18.5 || imc < 30){
-    console.log("Obesidade");
-}
-
-else{
-    console.log("Super gordão");
-}
+console.log(newArr);
