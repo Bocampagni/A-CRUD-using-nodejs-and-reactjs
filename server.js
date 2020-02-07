29b5c02a8a2a8d98e32c6e245aa0572d287d731d
Fabio Bocampagni <a.bocampagni@gmail.com>
2020-02-06 21:33:38 -0300

    Cors added

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 //inciando o app
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 
 //iniciando o db
@@ -13,7 +15,7 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', {
     useUnifiedTopology: true
 })
 mongoose.set('useFindAndModify', false);
-requireDir('./src/models');
+requireDir('./src/models'); //Da require em todos os arquivos do caminho
 
 
 //Rotas"
