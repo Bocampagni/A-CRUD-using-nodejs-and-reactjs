16a770a676fa5da248ff230bd3e404eef8fb2d3e
Fabio Bocampagni <a.bocampagni@gmail.com>
2020-02-08 20:46:31 -0300

    Adding React

---
+++ react/huntweb/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header'
+import "./style.css"
+import api from './services/api'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />        
+    </div>
+  );
+}
+
+export default App;
