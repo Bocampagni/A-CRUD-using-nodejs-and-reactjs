b0afb4f73598b91f3103943115cf4fe06ef3fe4f
Fabio Bocampagni <a.bocampagni@gmail.com>
2020-02-09 21:46:39 -0300

    React done

--- react/huntweb/src/App.js
+++ react/huntweb/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import Header from './components/Header'
-import "./style.css"
-import api from './services/api'
+import Header from './components/Header';
+import "./style.css";
+import api from './services/api';
+import Main from './pages/main';
+import Routes from './routes';
+
 
 function App() {
   return (
     <div className="App">
-      <Header />        
+      <Header /> 
+      <Routes />       
     </div>
   );
 }
